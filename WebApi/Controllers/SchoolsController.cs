1645f7398492997e5419392a89923476f34b656a
Khaled Elhanan <deathnotecode1@gmail.com>
2025-12-24 13:05:19 +0200

    feat(api): add SchoolsController command endpoints (create, update, delete)

---
+++ WebApi/Controllers/SchoolsController.cs
@@ -0,0 +1,49 @@
+﻿using Application.Features.Schools;
+using Application.Features.Schools.Commands;
+using Infrastructure.Constants;
+using Infrastructure.Identity.Auth;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class SchoolsController : BaseApiController
+    {
+        [HttpPost("add")]
+        [ShouldHavePermission(SchoolAction.Create, SchoolFeature.Schools)]
+        public async Task<IActionResult> CreateSchoolAsync([FromBody] CreateSchoolRequest createSchoolCommand)
+        {
+            var response = await Sender.Send(new CreateSchoolCommand { CreateSchool = createSchoolCommand });
+            if (response.IsSuccessful)
+            {
+                return Ok(response);
+            }
+            return BadRequest(response);
+
+        }
+        [HttpPut("update")]
+        [ShouldHavePermission(SchoolAction.Update, SchoolFeature.Schools)]
+        public async Task<IActionResult> UpdateSchoolAsync([FromBody] UpdateSchoolRequest updateSchoolCommand)
+        {
+            var response = await Sender.Send(new UpdateSchoolCommand { UpdateSchool = updateSchoolCommand });
+            if (response.IsSuccessful)
+            {
+                return Ok(response);
+            }
+            return NotFound(response);
+        }
+
+        [HttpDelete("{schoolId}")]
+        [ShouldHavePermission(SchoolAction.Delete, SchoolFeature.Schools)]
+        public async Task<IActionResult> DeleteSchoolAsync(int schoolId)
+        {
+            var response = await Sender.Send(new DeleteSchoolCommand { SchoolId = schoolId });
+            if (response.IsSuccessful)
+            {
+                return Ok(response);
+            }
+            return NotFound(response);
+        }
+    }
+}
